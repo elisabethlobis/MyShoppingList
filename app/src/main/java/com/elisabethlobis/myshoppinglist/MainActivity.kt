874c662d3ca826f04a086b40276a7563863ff7ef
elisabethlobis <elisabeth.lobis12@gmail.com>
2024-06-01 16:55:36 +0200

    shopping list

--- app/src/main/java/com/elisabethlobis/myshoppinglist/MainActivity.kt
+++ app/src/main/java/com/elisabethlobis/myshoppinglist/MainActivity.kt
@@ -1,59 +1,79 @@
 package com.elisabethlobis.myshoppinglist
+// Mit Package wird die App im Store identifiziert.
 
+// Import werden die  Daten, die man braucht aus Android importiert
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.text.InputType
+import android.widget.AdapterView.OnItemLongClickListener
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.elisabethlobis.myshoppinglist.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
+
+// Private -> gilt nur fuer diese Klasse
+// lateinit -> die beteffende Variable wird nicht sofort definiert
+// Wert nach Doppelpunkt -> Wie verhaelt sich die betreffende Variable?
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var lvTodoList: ListView
+    private lateinit var shoppingItem: ArrayList<String>
+    private lateinit var fab: FloatingActionButton
+    private lateinit var itemAdapter: ArrayAdapter<String>
 
+    // Override fun on create bedeutet, dass der Code innerhalb der Klammer beim Start der App ausgefuehrt wird.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        // Variablen werden mitteinander verknuepft
+        lvTodoList = findViewById(R.id.lvTodoList)
+        fab = findViewById(R.id.fab)
+        shoppingItem = ArrayList()
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        shoppingItem.add("Apfel")
+        shoppingItem.add("Banane")
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+        itemAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            shoppingItem
+        )
+
+        lvTodoList.adapter = itemAdapter
+
+        lvTodoList.setOnItemLongClickListener(OnItemLongClickListener { arg0, arg1, pos, id ->
+            shoppingItem.removeAt(pos)
+            itemAdapter.notifyDataSetChanged()
+            Toast.makeText(applicationContext, "Element gelöscht!", Toast.LENGTH_SHORT).show()
+            true
+        })
+
+        fab.setOnClickListener {
+            var builder = AlertDialog.Builder(this)
+            builder.setTitle("Hinzufügen")
+
+            var input = EditText(this)
+            input.hint = "Text eingeben"
+            input.inputType = InputType.TYPE_CLASS_TEXT
+
+            builder.setView(input)
+
+            builder.setPositiveButton("OK") { dialog, which ->
+                shoppingItem.add(input.text.toString())
+            }
+
+            builder.setNegativeButton("Abbrechen") { dialog, which ->
+                Toast.makeText(applicationContext, "Abgebrochen", Toast.LENGTH_SHORT).show()
+            }
+            builder.show()
         }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
